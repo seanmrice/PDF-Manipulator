6404260486cb7de3ae1afe4b27d254495f569412
Sean Rice <sean.m.rice@gmail.com>
2022-04-14 21:36:44 -0700

    Merge remote-tracking branch 'origin/main'

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
-import os
-import re
+from pathlib import Path
 from docx2pdf import convert
-import PyPDF2
 
 
-def convert_d2p(folder_in, folder_out):
+def convert_d2p(file_in):
     try:
-        convert(folder_in, folder_out)
+        # file_out = open(file_out, "w")
+        convert(file_in)
+        # file_out.close()
     except NameError:
         return print("Error: ", NameError)
     finally:
         return True
 
 
-folder_to_convert = os.listdir("/Users/sean/Convert")
-folder_to_output_to = ("/Users/sean/Convert")
-for item in folder_to_convert:
-    if re.search(".docx", item):
-        print(item)
-        convert_d2p(item, folder_to_output_to)
+folder_to_convert = "/Users/sean/Convert"
+files_to_convert = list(Path(folder_to_convert).glob("*.docx"))
+print(files_to_convert)
+file_to_output_to = "/Users/sean/Convert/"
+
+for item in files_to_convert:
+    convert_d2p(item)
